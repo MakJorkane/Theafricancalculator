da68d9ce2230fea6c7cba7dc697d628098007769
MakJorkane <jcmorkane@student.christscollege.com>
2026-05-28 15:31:20 +1200

    Implement cowsay horribly

--- Diddi.py
+++ Diddi.py
@@ -3,11 +3,12 @@ import math
 import statistics
 import random
 import time
+import cowsay
 
 
 def theTOS():
+    print("Welcome to thy calculator, currently inputs will be from the CLI, if you want to perform certain functions eg the mean of a value you just type mean")
     while True:
-        print("Welcome to thy calculator, currently inputs will be from the CLI, if you want to perform certain functions eg the mean of a value you just type mean")
         time.sleep(2)
         print("Read all the instructions on the paper, do you accept the calculator TOS?  (INPUT Y OR N case sensitive)")
         TOS = str(input("- >").capitalize().strip()) # Case sensitive ppl
@@ -18,27 +19,27 @@ def theTOS():
             print("Ok u cant use the calculator turn me on again if u feel like accepting the TOS")
             return TOS
         else:
-            break # This prevents it from looping again so pls dont remove
+            pass # This prevents it from looping again so pls dont remove
         
 
 def basic():
     while True:
         try:  # Keep this in the try statement to handle errors
+
+        except SyntaxError or NameError or TypeError:   # Cleaner
+            print(f"Pls do proper inputs")
+
+        calc = input("Enter ur calc here -> ")
+
+        if calc == "pet":
+                print("We will activate cowsay nwo!11")
+
             
-            calc = input("Enter ur calc here -> ")
-            print(f"Ur answer would be: {eval(calc)}") # Eval allows code execution, will try cleanse this later
-        except SyntaxError:
-            print(f"Your input of {calc} is incorrect so if u use 3x3 do 3*3")
-            continue
-        except NameError:
-            print(f"Your input of {calc} is stupid and is probaly using non-integer numbers")
-            continue
-        except TypeError:
-            print(f"Your input of {calc} contains brackets and sadly the integer cant handle them but soon the calculator will")
-            continue
-        
+        else:
+                print(f"Ur answer would be: {eval(calc)}") # Eval allows code execution, will try cleanse this later
+                
 
-
+    
 
 rootTOS = theTOS()
 
