7faf6d04db1015e98d1cb2600d7b2a1eb63c48ce
MakJorkane <jcmorkane@student.christscollege.com>
2026-06-08 09:51:03 +1200

    Implented more pets with if and elif
    Extremly optimized code

--- Diddi.py
+++ Diddi.py
@@ -21,7 +21,6 @@ def theTOS():
         else:
             pass # This prevents it from looping again so pls dont remove
         
-
 def basic():
     while True:
         try:
@@ -36,6 +35,7 @@ def basic():
 
             if 'pet' in calc:
                 print("We will activate cowsay nwo!11")
+                moowoo = the_cowsay(calc)
 
                 
             else:
@@ -44,9 +44,6 @@ def basic():
         except (SyntaxError, NameError , TypeError):   # Cleaner
             print(f"Pls do proper inputs")
             continue
-
-
-
 
 def the_replacer(ineedtobereplaced):
 
@@ -65,23 +62,88 @@ def the_replacer(ineedtobereplaced):
                 print("U hit (÷) if in the replacer")
                 thanksforeplacingme = ineedtobereplaced.replace("÷", "/")
                 return(thanksforeplacingme)
+        
+        elif 'pet' in ineedtobereplaced:
+             print("Wow time 2 go to cowsay")
+             the_cowsay(ineedtobereplaced)
+
+
             
         else:
                 print("U hit else")
                 return(ineedtobereplaced)
-                            
-            
-            
+        
+def the_cowsay(iwanttomoo):
+     print("Ok so basically u can pick a pet so which one do u want")
+     time.sleep(1)
+     print(f"U can pick from {cowsay.char_names}")
+     print(f"{iwanttomoo}")
+     time.sleep(5)
+     pickthypet = input("What pet u lowkey what???")
+
+     if 'beavis' in pickthypet:
+        print("U picked beavis")
+    
+     elif 'cheese' in pickthypet:
+        print("U picked cheese")
+    
+     elif 'cow' in pickthypet:
+        print("U picked cow")
+     
+     elif 'daemon' in pickthypet:
+        print("You picked daemon")
+     
+     elif 'dragon' in pickthypet:
+        print("You picked the dragon")
+     
+     elif 'fox' in pickthypet:
+        print("You picked the fox")
+     
+     elif 'ghostbusters' in pickthypet:
+        print("You pick ghostbusters scary!11")
+
+     elif 'kitty' in pickthypet:
+        print("You picked the kitty")
+
+     elif 'meow' in pickthypet:
+        print("You picked the meow")
+
+     elif 'miki' in pickthypet:
+        print("You picked miki")
+    
+     elif 'octopus' in pickthypet:
+        print("You picked the ocotpus")
+
+     elif 'pig' in pickthypet:
+        print("You picked the pig")
+     
+     elif 'stegosaurus' in pickthypet:
+         print("You picked the stegosaurus")
+
+     elif 'stimpy' in pickthypet:
+         print("You picked the stimpy")
+
+     elif 'trex' in pickthypet:
+         print("You picked the trex")
+
+     elif 'turkey' in pickthypet:
+         print("You picked the turkey")
+
+     elif 'turtle' in pickthypet:
+         print("You picked the turtle")
+
+     elif 'tux' in pickthypet:
+          print("You picked tux which is like linux")
+         
+
+         
 
         
 
 
-            
-
-       
-
     
-
+     
+                        
 rootTOS = theTOS()
 
 if rootTOS == "N":
