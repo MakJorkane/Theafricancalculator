7b0dee650e1a42e485e81a6bae7222abfee1bc86
MakJorkane <jcmorkane@student.christscollege.com>
2026-08-04 11:48:05 +1200

    Added a tiny bit more handling for no Y or N
    Will use switches soon

---
+++ diddi.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cmath>
+
+int main(){
+   
+    std::string tos; // Tos Yes or NO!!!!!!!!!!!!!!
+
+    std::cout << "Welcome to thy calculator, currently inputs will be from the CLI, if you want to perform certain functions eg the mean of a value you just type mean" << '\n';
+    std::cout << "Read all the instructions on the paper, do you accept the calculator TOS?  (INPUT Y OR N case sensitive)" << '\n';
+
+    std::cin >> tos;
+
+    if (tos == "Y" || tos == "y" || tos == "Yes" || tos == "yes"){
+        std::cout << ("Good Job");
+    }
+    else if (tos == "N" || tos == "n" || tos == "No" || tos == "no"){ // Bad code could use captilization later and no whitespace but I'm lazy rn
+        std::cout << "Ok u cant use the calculator turn me on again if u feel like accepting the TOS";
+        return 0;
+    }
+    else{
+        std::cout << "Ok so u need valid inputs";
+    }
+
+    std::cout << "Time to enter your calc: ";
+    std::string userinput;  // Main input
+    
+    std::cin >> userinput;
+
+    if(userinput == "*"){ //Might use switches later because its better than 50 elifs
+
+
+    }
+
+
+
+
+
+
+
+
+    return 0;
+}
