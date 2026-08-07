ecfd0e3f0b7ded9e82ff20139dd7005f6b821941
MakJorkane <jcmorkane@student.christscollege.com>
2026-08-07 12:51:11 +1200

    Line 32 still has an error
    I added random number generator (psuedo random) which is useful for stats but for some weird reason it does not approve.

--- diddi.cpp
+++ diddi.cpp
@@ -1,7 +1,9 @@
-#include <iostream>
-#include <cmath>
+#include <iostream> // Idk main stuff
+#include <cmath> // For Math
+#include <random> // For random stuff
 
-int main(){
+
+int tosfunction(){
    
     bool thevalue = true;
 
@@ -12,36 +14,102 @@ int main(){
 
     std::cin >> tos;
 
-    while(thevalue){
-    if (tos == "Y" || tos == "y" || tos == "Yes" || tos == "yes"){
+    while(thevalue){ // Ts boolean
+    if (tos == "Y" || tos == "y" || tos == "Yes" || tos == "yes" || tos == "ja" || tos == "Ja"){
         std::cout << ("Good Job");
+        thevalue = false;
     }
-    else if (tos == "N" || tos == "n" || tos == "No" || tos == "no"){ // Bad code could use captilization later and no whitespace but I'm lazy rn
+    else if (tos == "N" || tos == "n" || tos == "No" || tos == "no" || tos == "nee" || tos == "Nee"){ // Bad code could use captilization later and no whitespace but I'm lazy rn
         std::cout << "Ok u cant use the calculator turn me on again if u feel like accepting the TOS";
         return 0;
     }
     else{
-        std::cout << "Ok so u need valid inputs";
-        main();
+        std::cout << "Ok so u need valid inputs" << '\n';
+        tosfunction();
     }
 }
+    std::cout << "This should output";
+    userinput();
+
+    return 0;
+}
+
+
+int userinput(){
+
+    int thyrandomnumber;
+    int maxvalue;
+    int minvalue;
+
+    srand(time(0));
+
+    thyrandomnumber = rand() % (maxvalue - minvalue + 1) + minvalue;
+    std::cout << "We are debugging random number" << thyrandomnumber;
+
+
+    std::string input;
 
     std::cout << "Time to enter your calc: ";
-    std::string userinput;  // Main input
+    std::getline (std::cin, input);  // Main input and we need to take in whitespace
+
+    int tempswitch;
+
+    // Big issue eval value does not exist in c++ because everything is compiled so we need alternative method
+    // One method could be grouping a bunch of existing integers and then executing them e.g 234234 * 43243 would group it as group1 * group2 
+
+    switch(tempswitch){
     
-    std::cin >> userinput;
+    
+    case 1:{
+        
+    }
 
-    if(userinput == "*"){ //Might use switches later because its better than 50 elifs
-
+    case 2:{
 
     }
 
+    case 3:{
+
+    }
+
+    case 4:{
+
+    }
+
+    case 5:{
+
+    }
+   
+    }
 
 
+    if(input.find('*')){ //Might use switches later because its better than 50 elifs
+    
+    std::cout << "We are now doing multiplicaton";
 
+    }
 
+    else if(input.find('/')){
+
+        std::cout << "We are now doing some division";
+    }
+
+        else if(input.find('+')){
+
+        std::cout << "We are now doing some addition";
+    }
+
+        else if(input.find('-')){
+
+        std::cout << "We are now doing some subtraction";
+    }
 
 
 
     return 0;
+
+
+
+
+
 }
